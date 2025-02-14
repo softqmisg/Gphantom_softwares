573562d884f73654b12e832793e543dc958ba53c
Mehdi Bagherimofidi <mehdi.bagherimofidi@yahoo.com>
2025-02-14 11:17:44 +0330

    change Address of dicom Add Generate  Plot Slice add Mojtaba file to other scripts

---
+++ python_scripts/tt.py
@@ -0,0 +1,9 @@
+import logging
+
+logging.basicConfig(level=logging.NOTSET,format='%(asctime)s:%(levelname)s: %(message)s')
+logging.debug("debug")
+logging.info("info")
+logging.warning("warning")
+logging.error("error")
+logging.critical("critical")
+# logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
